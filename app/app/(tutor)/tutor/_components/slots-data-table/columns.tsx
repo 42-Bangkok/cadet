72dbc9bd2dd2265a756c4b4f6450bc203b5b6f54
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2024-07-02 14:59:41 +0000

    feat: Update column headers in SlotsDataTable component

--- app/app/(tutor)/tutor/_components/slots-data-table/columns.tsx
+++ app/app/(tutor)/tutor/_components/slots-data-table/columns.tsx
@@ -54,25 +54,37 @@ export const columns: ColumnDef<InferSelectModel<typeof evaluationSlots>>[] = [
   },
   {
     accessorKey: "startDateTime",
-    header: "Starts",
+    header: "Date",
     cell: ({ row }) => {
-      return (row.getValue("startDateTime") as Date).toLocaleString(
+      return (row.getValue("startDateTime") as Date).toLocaleDateString(
         "en-US",
-        {}
+        {
+          day: "2-digit",
+          month: "2-digit",
+          year: "numeric",
+        }
       );
     },
   },
   {
     accessorKey: "startDateTime",
-    header: "Ends",
+    header: "Time",
     cell: ({ row }) => {
+      const start = (row.getValue("startDateTime") as Date).toLocaleString(
+        "en-US",
+        {
+          hour: "2-digit",
+          minute: "2-digit",
+        }
+      );
       const startDateTime = new Date(row.getValue("startDateTime") as Date);
       const endDateTime = new Date(startDateTime);
       endDateTime.setHours(startDateTime.getHours() + 1);
-      return endDateTime.toLocaleString("en-US", {
-        hour: "numeric",
-        minute: "numeric",
+      const end = endDateTime.toLocaleString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
       });
+      return `${start} ~ ${end}`;
     },
   },
 ];
