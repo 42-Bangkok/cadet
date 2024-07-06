94ab055f61018900ebf7a3d0852ad0f54dfc8318
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2024-07-06 14:11:04 +0000

    .

--- app/app/(dashboard)/dashboard/_components/booked-slots-cards/index.tsx
+++ app/app/(dashboard)/dashboard/_components/booked-slots-cards/index.tsx
@@ -8,8 +8,9 @@ import { alias } from "drizzle-orm/pg-core";
 export const BookedSlotsCards = async () => {
   const session = await auth();
   const evaluator = alias(users, "evaluator");
-
-  const res = await db
+  const nowPlus30 = new Date();
+  nowPlus30.setMinutes(nowPlus30.getMinutes() + 30);
+  const qs = await db
     .select()
     .from(evaluatees)
     .where(eq(evaluatees.userId, session!.user!.id!))
@@ -17,16 +18,16 @@ export const BookedSlotsCards = async () => {
       evaluationSlots,
       and(
         eq(evaluationSlots.id, evaluatees.evaluationSlotId),
-        gte(evaluationSlots.startDateTime, new Date())
+        gte(evaluationSlots.startDateTime, nowPlus30)
       )
     )
     .innerJoin(evaluator, eq(evaluator.id, evaluationSlots.evaluatorUserId));
-  if (res.length === 0) {
-    return <div>{"No booked slots"}</div>;
+  if (qs.length === 0) {
+    return <div>{"No booked slot(s)..."}</div>;
   }
   return (
     <div className="grid grid-cols-1 gap-4">
-      {res.map((slot) => (
+      {qs.map((slot) => (
         // TODO: Aggregate evaluatees
         // @ts-ignore
         <BookedSlotCard key={slot.evaluationSlot!.id} {...slot} />
