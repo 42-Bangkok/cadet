80f3d944139cef955251ce77e2e10ebb2487df8a
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2024-09-05 15:17:51 +0000

    feat: Add theme provider component
    This commit adds a new component called ThemeProvider, which wraps the NextThemesProvider from the next-themes library. The ThemeProvider component accepts props from the NextThemesProvider and renders its children. This change allows for easy theming of the application by providing a centralized theme provider component.

--- app/app/_components/nav-bar/index.tsx
+++ app/app/_components/nav-bar/index.tsx
@@ -1,4 +1,5 @@
 import { FortyTwoSignIn } from "@/components/auth/fortytwo-sign-in/";
+import { ThemeToggle } from "@/components/theme-toggle";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 
@@ -10,21 +11,19 @@ export const NavBar = ({ className }: INavBar) => {
   return (
     <nav
       className={cn(
-        "flex justify-between items-center backdrop-blur-sm p-1 bg-primary/20 sticky top-0 z-1000 shadow-violet-400 shadow-sm",
-        className,
+        "flex justify-between items-center backdrop-blur-sm p-1 sticky top-0 z-1000 shadow-sm border-b",
+        className
       )}
     >
-      <div className="flex items-center space-x-4 bg-white rounded p-1 pt-0 pb-0 shadow shadow-violet-300">
+      <div className="flex items-center space-x-4 rounded p-1 pt-0 pb-0 shadow ">
         <Link href="/">
           <span className="text-lg font-bold text-primary tracking-widest hover:text-primary/80 ">
             🔥 Cadet @ 42bangkok.com
           </span>
         </Link>
-        {/* <a href="/app" className="text-blue-600">
-          Dashboard
-        </a> */}
       </div>
       <div className="flex items-center space-x-4">
+        <ThemeToggle />
         <FortyTwoSignIn />
       </div>
     </nav>
--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { NavBar } from "./_components/nav-bar";
 import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,9 +21,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn(inter.className, "h-dvh p-0")}>
-        <NavBar className="mb-4 z-50" />
-        <div className="pl-4 pr-4">{children}</div>
-        <Toaster />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <NavBar className="mb-4 z-50" />
+          <div className="pl-4 pr-4">{children}</div>
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
