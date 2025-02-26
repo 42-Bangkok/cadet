e3a602268cca09e31064ea2288875d09aa45c5d4
Pumidol <pumidol.l@gmail.com>
2025-02-26 13:34:46 +0000

    chore: Simplify Next.js configuration by removing unused webpack settings

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // disables webpack.cache.PackFileCacheStrategy] Serializing big strings (261kiB) impacts deserialization performance (consider using Buffer instead and decode when needed)
-  webpack: (config) => {
-    config.infrastructureLogging = {
-      level: "error",
-    };
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
